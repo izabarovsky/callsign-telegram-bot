de9bb40fe144293690a5ca6b69c9f66f8cc29a2e
brunnen-g <qazabarovsky@gmail.com>
2024-11-22 08:59:57 +0200

    [CS-44] Increase repeater info size

---
+++ repeaters.sql
@@ -0,0 +1,9 @@
+INSERT INTO repeater(info, echolink) VALUES (E'RX/TX 438.375\nCTSS: 123.0Hz\nQTH - Борщагівка', true);
+
+INSERT INTO repeater(info, official, digital) VALUES (E'<b>R3 🔋</b>\nRX 145.675 / TX 145.075 (offset: -0.6)\nCTCSS: 88.5Hz', true, false);
+INSERT INTO repeater(info, official, digital) VALUES (E'<b>R76 🔋</b>\nRX 438.800 / TX 431.200 (offset -7.6)\nCTCSS: 88.5Hz\nQTH - Бровари', true, false);
+INSERT INTO repeater(info, official, digital) VALUES (E'<b>R81</b>\nRX 438.925 / TX 431.325 (offset -7.6)\nCTCSS: 88.5Hz\nQTH - Кловський узвіз', true, false);
+INSERT INTO repeater(info, official, digital) VALUES (E'<b>R85 🔋</b>\nТимчасово не працює! ⚒️\nRX 439.025 / TX 431.425 (offset -7.6)\nAnalog\nCTCSS: 88.5Hz\nDigital\nTS: 1 CC: 1\nTG: 25501 (Kyiv)', true, true);
+INSERT INTO repeater(info, official, digital) VALUES (E'<b>R89</b>\nRX 439.125 / TX 431.525 (offset -7.6)\nAnalog\nCTCSS: 88.5Hz\nDigital\nTS: 1 CC: 1\nTG: 25501 (Kyiv) 777 (local)\nQTH - Кловський узвіз', true, true);
+
+
