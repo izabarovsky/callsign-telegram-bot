c31f42d4318e58b0ed1d7e5502f3db1f8c5d2824
brunnen-g <qazabarovsky@gmail.com>
2024-11-22 11:21:05 +0200

    [CS-44] Add order by id to repeater queries

--- src/main/java/com/izabarovsky/callsign/telegram/bot/persistence/RepeaterRepository.java
+++ src/main/java/com/izabarovsky/callsign/telegram/bot/persistence/RepeaterRepository.java
@@ -8,19 +8,19 @@ import java.util.List;
 
 public interface RepeaterRepository extends JpaRepository<RepeaterEntity, Long> {
 
-    @Query("select u from RepeaterEntity u where u.official = true")
+    @Query("select u from RepeaterEntity u where u.official = true order by u.id")
     List<RepeaterEntity> findOfficial();
 
-    @Query("select u from RepeaterEntity u where u.official = false and u.echolink = false and u.simplex = false")
+    @Query("select u from RepeaterEntity u where u.official = false and u.echolink = false and u.simplex = false order by u.id")
     List<RepeaterEntity> findNonOfficial();
 
-    @Query("select u from RepeaterEntity u where u.simplex = true")
+    @Query("select u from RepeaterEntity u where u.simplex = true order by u.id")
     List<RepeaterEntity> findParrots();
 
-    @Query("select u from RepeaterEntity u where u.echolink = true")
+    @Query("select u from RepeaterEntity u where u.echolink = true order by u.id")
     List<RepeaterEntity> findEcholink();
 
-    @Query("select u from RepeaterEntity u where u.digital = true")
+    @Query("select u from RepeaterEntity u where u.digital = true order by u.id")
     List<RepeaterEntity> findDigital();
 
 }
